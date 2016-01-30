afddc088f01903296135690b9767c69f08b2add2
juanruocco <juanpabloruocco@gmail.com>
2016-01-29 19:13:51 -0500

    preload images

---
+++ js/init.js
@@ -0,0 +1,9 @@
+img1 = new Image();
+img2 = new Image();
+img3 = new Image();
+img4 = new Image();
+
+img1.src = "assets/tomas/forward.png";
+img2.src = "assets/tomas/backward.png";
+img3.src = "assets/tomas/jump.png";
+img4.src = "assets/tomas/light_boxing.png";
